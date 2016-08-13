d352aa4a388a69458c782309247f10df66c43109
Elior Avramoviz <elioravr@gmail.com>
2016-08-13 12:51:54 +0300

    Add empty list component

--- src/components/CategoriesList.js
+++ src/components/CategoriesList.js
@@ -1,12 +1,18 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 
 import Category from './Category';
+import EmptyList from './EmptyList';
 
 export default class CategoriesList extends Component {
   renderCategories() {
-    return this.props.categories.map((category) => {
-      return <Category key={category._id} category={category} />
-    });
+    if (_(this.props.categories).isEmpty()) {
+      return <EmptyList entityName="categories" />
+    } else {
+      return this.props.categories.map((category) => {
+        return <Category key={category._id} category={category} />
+      });
+    }
   }
 
   render() {
---
+++ src/components/EmptyList.js
@@ -0,0 +1,26 @@
+import React, { Component, PropTypes } from 'react';
+
+import IconArrow from 'material-ui/svg-icons/navigation/arrow-upward';
+
+export default class EmptyList extends Component {
+  render() {
+    this.iconStyle = {
+      height: '40px',
+      width: '40px',
+      color: '#b3b3b3'
+    };
+
+    return (
+      <div className="empty-view-container">
+        <span>You have no {this.props.entityName} yet. Please add some.</span>
+        <div className="arrow-container">
+          <IconArrow style={this.iconStyle} />
+        </div>
+      </div>
+    );
+  }
+}
+
+EmptyList.propTypes = {
+  entityName: PropTypes.string.isRequired
+};
